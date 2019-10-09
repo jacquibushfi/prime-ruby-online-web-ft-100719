ba64f46193d1ddb8e7a258b3969a74b35f7270ab
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-09 14:06:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,18 @@
- def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
-  true
-end 
+# def prime?(integer)
+#  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+#  true
+# end 
+# end
+
+
+def prime?(value)
+  if value <= 1 
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+   end
+  true  
 end
-
-
